f31de3b8f7ddfa35fb53a8eb28c4fcae2a0bf94e
Vaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2022-10-06 20:40:25 +0530

    "task done"

--- pages/index.js
+++ pages/index.js
@@ -28,14 +28,14 @@ export default function Home() {
     <div>
       <div className='hidden sm:flex h-[100vh] bg-gradient-to-b from-[#af67e9] to-[#6565e7] justify-center items-center'>
           <div className='bg-white bg-cover rounded-2xl w-3/5 h-3/4 shadow-lg flex'>
-            <div className='w-1/2'>
-              <img src='/images/bg-pattern-desktop.svg' className='absolute left-48 top-28 h-2/3'></img>
-              <img src='/images/illustration-woman-online-desktop.svg' className='absolute top-36 left-48 h-1/2'></img>
-              <img src='/images/illustration-box-desktop.svg' className='absolute top-64 left-48 h-32'></img>
+            <div className='w-1/2 relative overflow-hidden'>
+              <img src='/images/bg-pattern-desktop.svg' className='absolute left-[-50px] top-[10%]'></img>
+              <img src='/images/illustration-woman-online-desktop.svg' className='absolute top-[15%] left-[-50px]'></img>
+              <img src='/images/illustration-box-desktop.svg' className='absolute top-40 left-[-80px]'></img>
             </div>
-            <div className='w-1/2 py-2 mt-8 flex flex-col'>
+            <div className='w-1/2 pt-2 pb-8 mt-12 flex flex-col overflow-auto'>
               <div className='text-3xl font-bold'>FAQ</div>
-              <div className='mt-4'>
+              <div className='mt-8'>
                 {data.map((i)=>{
                   return(
                     <Question 
@@ -48,13 +48,15 @@ export default function Home() {
             </div>
           </div>
       </div>
-      <div className='h-[100vh] flex sm:hidden bg-gradient-to-b from-[#af67e9] to-[#6565e7] justify-center items-center overflow-clip'>
-        <img src='/images/illustration-woman-online-mobile.svg' className='absolute top-20 h-40'></img>  
-        <img src='/images/bg-pattern-desktop.svg' className='absolute top-16 h-52'></img>  
-        <div className='h-2/3 bg-white w-4/5 rounded-2xl mt-12'>
+      <div className='h-[100vh] flex sm:hidden bg-gradient-to-b from-[#af67e9] to-[#6565e7] justify-center items-center overflow-auto'>
+        <div className='bg-white w-4/5 rounded-2xl mt-20 pb-8'>
+          <div className='relative bg-black'>
+            <img src='/images/illustration-woman-online-mobile.svg' className='absolute top-[-80px] left-[20%] h-40'></img>  
+            <img src='/images/bg-pattern-desktop.svg' className='absolute top-[-100px] left-[20%] h-52'></img>  
+          </div>
           <div className='mt-32 px-2'>
             <div className='text-center text-3xl font-bold'>FAQ</div>
-            <div className='mt-8'>
+            <div className='mt-8 overflow-auto'>
                 {data.map((i)=>{
                   return(
                     <Question 
